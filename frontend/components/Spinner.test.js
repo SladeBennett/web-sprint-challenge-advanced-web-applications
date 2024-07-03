3805150d0674d4225e40a522eabed292060370a9
SladeBennett <slade8colt@gmail.com>
2024-07-03 16:54:39 -0600

    Complete

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,6 +1,17 @@
 import Spinner from "./Spinner"
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import React from "react"
+
 //$ Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+test('spinner displays correctly', () => {
+  let on = true
+  render(<Spinner on={on} />)
+  expect(screen.queryByText('Please wait...')).toBeInTheDocument()
+})
+test('spinner displays incorrectly', () => {
+  let on = true
+  render(<Spinner on={!on} />)
+  expect(screen.queryByText('Please wait...')).not.toBeInTheDocument()
 })
